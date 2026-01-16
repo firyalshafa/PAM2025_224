74427c6d7a6c6dd82e7c5c67cc7c94fb1bbfe9f2
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2026-01-16 20:24:25 +0700

    ganti IP untuk mealkukan tes

--- app/src/main/java/com/example/peyewaan/utils/Constants.kt
+++ app/src/main/java/com/example/peyewaan/utils/Constants.kt
@@ -5,4 +5,4 @@ package com.example.peyewaan.utils
 object Constants {
 
     // Gunakan IP terbaru dari CMD kamu
-    val BASE_URL = "http://192.168.0.101/penyewaan/"}
+    val BASE_URL = "http://192.168.0.103/penyewaan/"}
